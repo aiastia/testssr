cd0b914835cda42e04420d3ef633fcf3ee487589
未来 <aiastia@users.noreply.github.com>
2020-02-07 14:58:31 +0800

    Update userapiconfig.py

--- userapiconfig.py
+++ userapiconfig.py
@@ -1,17 +1,17 @@
 ﻿# Config
-NODE_ID = 0
+NODE_ID = 1
 
 # hour,set 0 to disable
-SPEEDTEST = 6
+SPEEDTEST = 24
 CLOUDSAFE = 1
-ANTISSATTACK = 0
+ANTISSATTACK = 1
 AUTOEXEC = 0
 
-MU_SUFFIX = 'zhaoj.in'
+MU_SUFFIX = 'cloudflare.com'
 MU_REGEX = '%5m%id.%suffix'
 
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
-API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
+API_INTERFACE = 'glzjinmod'  # glzjinmod, modwebapi
 
 WEBAPI_URL = 'https://zhaoj.in'
 WEBAPI_TOKEN = 'glzjin'
